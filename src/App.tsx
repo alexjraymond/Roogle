b36de62441639487fecdd5e733874fc826e12811
Alex Raymond <alexjacobraymond@gmail.com>
2024-04-04 11:53:20 -0700

    reddit search works!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import { useState } from 'react'
 import './App.css'
 import roogleLogo from './img/ROOGLE_LOGO_FINAL.png'
 import { GoSearch } from "react-icons/go";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const searchInput = document.getElementById('searchBar') as HTMLInputElement | null;
+    if (searchInput && searchInput.value.trim()) {
+      const searchQuery = searchInput.value.trim();
+      const fullQuery = `${searchQuery} reddit`;
+      window.location.href = `https://www.google.com/search?q=${encodeURIComponent(fullQuery)}`;
+    } else {
+      console.log('Please enter a search query.');
+    }
+  };
+
 
   return (
     <>
-      <div>
-        < img src={roogleLogo} />
+      <div className='logo-container'>
+        < img src={roogleLogo} className='roogle-logo' />
 
       </div>
+      <form onSubmit={handleFormSubmit}>
       <div className='search-container'>
+
       <input type='search' id='searchBar'/>
-      <label htmlFor="searchBar" className="search-button" onClick={() => setCount((count) => count + 1)}>
+      <label htmlFor="searchBar" className="search-button">
           <GoSearch />
         </label>
       </div>
-      <div className="card">
-          search is {count}
-      </div>
+      </form>
 </>
   )
 }
